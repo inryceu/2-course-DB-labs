1ab7874ce74c399fd08c5ef68fff58dab681724c
yurii <yurachoruk123@gmail.com>
2025-11-10 18:41:40 +0200

    insert_into_users.sql

---
+++ Lab3/SQL/insert_into_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, email, password_hash, age, region, avatar)
+VALUES ('jellyfish', 'jellyjelly@example.com', '$2b$10$O1V9wFZzQ3sQOa2Kq4zZ9Ox0E8ZTRHfKz7yKlkQq5T9xX9eWzn0eG', 14, 'LT', 'https://example.com/jellyfish.png');
